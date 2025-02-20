b235a65a796909fc7d5bc076299ffb6fc50a6223
Simran Gurung <119354147+Simrannye@users.noreply.github.com>
2025-02-20 21:34:28 +0545

    login error fixed

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './log.css';
-import Signup from "./components/Signup";
+
 
 const Login = () => {
   const [email, setEmail] = useState('');
